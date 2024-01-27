1a6ab75637eecbccbc3001553c1b1e1c5ba96e27
Sanskar Mishra <sanskarmishra@gmail.com>
2024-01-27 12:56:26 -0800

    updated api request so that you can give the chat bot a prompt and it will return a response

--- openai-test.js
+++ openai-test.js
@@ -1,10 +1,16 @@
+const dotenv = require("dotenv").config()
+
 const OpenAI = require("openai");
 
-const openai = new OpenAI();
+const API_KEY = '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';
+
+const openai = new OpenAI(API_KEY);
+
+console.log("REACHED")
 
 async function main() {
   const completion = await openai.chat.completions.create({
-    messages: [{ role: "system", content: "You are a helpful assistant." }],
+    messages: [{ role: "system", content: "how was your day (one word only" }],
     model: "gpt-3.5-turbo",
   });
 
