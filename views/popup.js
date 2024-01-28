004b69dd717bddfed2ac4c296eef68a1e5cefa91
Ashley Zhou <ashlejz1@uci.edu>
2024-01-27 22:19:41 -0800

    edited popup: center, font, color scheme, & added background img

--- views/popup.js
+++ views/popup.js
@@ -1,9 +1,9 @@
-// script.js
+//display slider value
 var slider = document.getElementById("pa-slider");
 var output = document.getElementById("sliderValue");
-output.innerHTML = slider.value;
 
 slider.oninput = function() {
     output.innerHTML = this.value;
 }
+
 
